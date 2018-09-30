1c7ae53dee21593f53905ea8e89464e5f742f87d
critsmet <36425627+critsmet@users.noreply.github.com>
2018-09-30 12:40:17 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {}
+  let maxChar = ''
+  let max = 0
+
+  for (let char of str){
+    obj[char] = obj[char]++ || 1
+  }
+
+  for (let char in obj){
+    if (obj[char] > max){
+      maxChar = char
+      max = obj[char]
+    }
+  }
+  return maxChar
+}
 
 module.exports = maxChar;
